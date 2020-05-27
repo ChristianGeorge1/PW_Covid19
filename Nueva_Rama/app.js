6fdbb5832e31478bd5bfba40dce88b7670fc6f0a
ChristianGeorge1 <ccastellanos4@ucol.mx>
2020-05-26 20:16:23 -0500

    Tarea

--- Nueva_Rama/app.js
+++ Nueva_Rama/app.js
@@ -3,17 +3,6 @@ const bodyParser = require('body-parser');
 const app = express(); 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://admin:admin@beducluster-eiuzk.mongodb.net/test?retryWrites=true&w=majority');
-
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-    userName: String,
-    password: String
-});
-
 
 const port = 4000;
 
@@ -38,26 +27,11 @@ app.get('/person/:id', (req, res)=>{
 })
 
 //?Método post 
-app.post('/person', urlencodedParser, (req, res) => {
-    // res.send('Thanks');
-    console.log(req.body.userName);
-    // console.log(req.body.lastname);
-    //aqui empieza la busqueda
-    var user = mongoose.model('User', userSchema);
-    user.find({ userName: req.body.userName }, function (err, data) {
-        // doc is a Document
-        if (err) {
-            console.log('Hubo un error');
-        } else {
-            if (data.length > 0) {
-                // console.log(data);
-                res.render('results', { data });
-                // console.log(doc);
-            } else { res.send('no hay coincidencias para el criterio de busqueda'); }
-        }
-    });
-});
-
+app.post('/person', urlencodedParser, (req, res)=>{
+    res.send('Thanks'); 
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+})
 
 //?Método post 2
 app.post('/personJson', jsonParser, (req, res)=>{
